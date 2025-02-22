857ac0eb2fd6fc62030dc2fcff287650e83e98d7
svetlanachoko <svetlana02072007@gmail.com>
2025-02-22 13:21:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,19 @@
 import telebot
 import os
+from flask import Flask, request
 
 TOKEN = os.getenv("TOKEN")
 bot = telebot.TeleBot(TOKEN)
+app = Flask(__name__)
+
+@app.route(f"/{TOKEN}", methods=["POST"])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Бот работает!", 200
 
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
@@ -10,7 +21,8 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "🎩 Судьба загадочна... но шоколад всегда помогает!")
+    bot.reply_to(message, "🔮 Судьба загадочна... но шоколад всегда помогает!")
 
-bot.infinity_polling()
-
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
