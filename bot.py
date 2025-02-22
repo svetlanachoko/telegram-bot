865c9cb60623024b6edd43ece4f0c9ae538285ce
svetlanachoko <svetlana02072007@gmail.com>
2025-02-22 11:35:16 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,15 @@
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=["start", "help"])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот-прорицатель. Хочешь узнать своё предсказание? Напиши мне!")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, "🎩 Судьба загадочна... но шоколад всегда помогает!")
+
+bot.polling()
